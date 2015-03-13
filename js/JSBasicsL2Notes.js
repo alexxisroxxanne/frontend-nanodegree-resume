5fa15096941abf014cbfade13db2bd6a2a150af9
Alexxis <AlexxisJohnson@Alexxis-iMac.local>
2015-03-13 15:06:41 -0700

    Forgot to actually save last commit. Awkward

--- js/JSBasicsL2Notes.js
+++ js/JSBasicsL2Notes.js
@@ -10,3 +10,54 @@ function locationizer(work_obj) {
 
 	return locationArray;
 }
+
+// LOL Everything. is an object. in JS
+
+projects.display = function() {
+	// Encapsulates function in projects
+}
+
+function displayWork() {
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace
+			("%data%", work.jobs[job].employer);
+		// $("#employer").append(formattedJob);
+		// $("work-entry:last").append(formattedJob);
+		// formattedJob = HTMLworkEmployer.replace("%data", work.jobs.employer[1]);
+		// $("#employer").append(formattedJob);
+		// $("work-entry:last").append(formattedJob);
+
+		var formattedTitle = HTMLworkTitle.replace
+			("%data%", work.jobs[job].title);
+		// $("#title").append(formattedJob);
+		// $("work-entry:last").append(formattedJob);
+		// formattedJob = HTMLworkTitle.replace("%data", work.jobs.title[1]);
+		// $("#title").append(formattedJob);
+
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedPosition = HTMLworkLocation.replace
+			("%data%", work.jobs[job].position);
+		$(".work-entry:last").append(formattedPosition);
+
+		var formattedDate = HTMLworkDates.replace
+			("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDate);
+
+		var formattedLocation = HTMLworkLocation.replace
+			("%data%", work.jobs[job].location);
+		$(".work-entry:last").append(formattedLocation);
+
+		var formattedDescription = HTMLworkDescription.replace
+			("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+
+	// var formattedPositionDateDescription = formattedPosition +
+	//	formattedDate + formattedDescription;
+
+	// $(".work-entry:last").append(formattedPositionDateDescription);
+	}
+}
